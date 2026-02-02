9f5bb8fab4f21392a2b7e86fcf9fbb7e7d4cea48
Anagha Nirgude <anagha.nirgude1@gmail.com>
2026-02-02 18:54:12 +0530

    custom exception

--- src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -26,6 +26,10 @@ public class CategoryServiceImpl implements CategoryService {
 
     @Override
     public List<Category> getAllCategories() {
+        List<Category> categories = categoryRepository.findAll();
+        if(categories.isEmpty()){
+            throw new APIException("No category created yet!!!");
+        }
         return categoryRepository.findAll();
     }
 
