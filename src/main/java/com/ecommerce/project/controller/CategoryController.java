1850ec41a86053ead6340fd565ba7799d5c76497
Anagha Nirgude <anagha.nirgude1@gmail.com>
2026-02-02 18:56:21 +0530

    controller cleanup

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -6,7 +6,6 @@ import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 
@@ -34,26 +33,15 @@ public class CategoryController {
 
     @DeleteMapping(path = "/admin/categories/{categoryId}")
     public ResponseEntity<String> deleteCategory(@PathVariable Long categoryId){
-        try {
             String status = categoryService.deleteCategory(categoryId);
             //return new ResponseEntity<>(status, HttpStatus.OK);
             //return ResponseEntity.ok(status);
             return ResponseEntity.status(HttpStatus.OK).body(status);
-        }
-        catch (ResponseStatusException rse){
-            return new ResponseEntity<>(rse.getReason(), rse.getStatusCode());
-        }
     }
 
     @PutMapping(path = "/public/categories/{categoryId}")
     public ResponseEntity<String> updateCategory(@PathVariable Long categoryId, @RequestBody Category category){
-
-        try {
             Category updatedCategory = categoryService.updateCategory(category, categoryId);
             return ResponseEntity.status(HttpStatus.OK).body("Category with categoryId " + categoryId + " updated successfully");
-        }
-        catch (ResponseStatusException rse){
-            return new ResponseEntity<>(rse.getReason(), rse.getStatusCode());
-        }
     }
 }
